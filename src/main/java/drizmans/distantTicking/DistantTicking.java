d550f4f2c5e7a2e9c0dbda690488b54632238c23
Jack Jenkins <jackjenkins@penguin.local>
2025-06-23 17:51:29 +0100

    Initial commit

---
+++ src/main/java/drizmans/distantTicking/DistantTicking.java
@@ -0,0 +1,17 @@
+package drizmans.distantTicking;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DistantTicking extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
